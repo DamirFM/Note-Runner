a6f63bb343488ae7e8963f9cd3a432e9eeadaa5a
DamirFM <yakupovdamir93@gmail.com>
2023-12-14 20:10:30 -0500

    updated server.js

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/pages/404.html'))
 );
 
 app.listen(PORT, () =>
